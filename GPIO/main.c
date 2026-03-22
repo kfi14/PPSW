3bfc9e50cc179ef44016416714b0b6241d6e3663
unknown <jakub.ficer@gmail.com>
2026-03-22 15:12:19 +0100

    zadanie 10

--- GPIO/main.c
+++ GPIO/main.c
@@ -1,6 +1,6 @@
 #include <LPC21xx.H>
 #define LED0_bm 0x00010000
-
+#define LED3_bm 0x00080000
 
 void Delay(unsigned int uiMiliseconds) 
 {
@@ -16,13 +16,13 @@ void Delay(unsigned int uiMiliseconds)
 
 int main()
 {
-    IO1DIR = LED0_bm;
+    IO1DIR = LED3_bm;
 
     while(1)
         {
-        IO1SET = LED0_bm;
+        IO1SET = LED3_bm;
         Delay(50);
-        IO1CLR = LED0_bm;
+        IO1CLR = LED3_bm;
         Delay(50);
     }
 }
