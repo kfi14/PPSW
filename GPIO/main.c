1b0b286b540faeb789c4c53022fc5a575f4dacef
Jakub Ficer <jakub.ficer@gmail.com>
2026-03-24 10:01:50 +0100

    automaty 2

--- GPIO/main.c
+++ GPIO/main.c
@@ -16,25 +16,46 @@ void Delay(unsigned int uiMiliseconds)
 }
 
 
-int main()
-{
-					LedInit();
-					KeyboardInit();
+int main(void){
 	
-					while(1)
-					{
-						switch(eKeyboardRead()){
-						
-						case BUTTON_1:
-							LedStepRight();
-						break;
-						
-						case BUTTON_2:
-							LedStepLeft();
-						break;
-						
-						default:
-						break;
-						}					
-					}
+			enum LedState{STATE0, STATE1, STATE2, STATE3, STATE4, STATE5};
+			enum LedState eLedState = STATE0;
+
+			LedInit();
+
+			while(1){
+				switch(eLedState){
+					case STATE0:
+						LedStepLeft();
+						eLedState = STATE1;
+					break;
+
+					case STATE1:
+						eLedState = STATE2;
+						LedStepLeft();
+					break;
+					
+					case STATE2:
+						LedStepLeft();
+						eLedState = STATE3;
+					break;
+					
+					case STATE3:
+						LedStepRight();
+						eLedState = STATE4;
+					break;
+					
+					case STATE4:
+						LedStepRight();
+						eLedState = STATE5;
+					break;
+					
+					case STATE5:
+						LedStepRight();
+						eLedState = STATE0;
+					break;
+					
+					Delay(250);
+			}	
+		}
 }
