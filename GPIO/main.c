6da65076cac46b5dc32e22a8cd9282a523b76d6e
unknown <jakub.ficer@gmail.com>
2026-03-31 23:52:48 +0200

    timery 2

--- GPIO/main.c
+++ GPIO/main.c
@@ -6,15 +6,11 @@
 
 int main(void){
 	
-			enum LedState{LED_MOVING_LEFT, LED_MOVING_RIGHT, LED_STOPPED};
-			enum LedState eLedState = LED_STOPPED;
-
 			LedInit();
-			InitTimer0();
-
+			InitTimer0Match0(250000);
+	
 			while(1){
-				
 				LedStepLeft();
-				WaitOnTimer0(250000);
+				WaitOnTimer0Match0();
 		}
 }
--- GPIO/timer.c
+++ GPIO/timer.c
@@ -3,11 +3,16 @@
 
 #define Counter_Enable (1<<0)
 #define Counter_Reset (1<<1)
+#define Interrupt_MR0 (1<<0)
+#define Reset_MR0 (1<<1)
+#define Stop_MR0 (1<<2)
+#define MR0_Interrupt_Flag (1<<0)
 
 
 void InitTimer0(void){
 	T0TCR = Counter_Enable;
 }
+
 
 void WaitOnTimer0(unsigned int uiTime){
 	T0TCR = T0TCR | Counter_Reset;
@@ -15,5 +20,20 @@ void WaitOnTimer0(unsigned int uiTime){
 	
 	while(T0TC <= uiTime*15){}
 }		
+
+
+void InitTimer0Match0(unsigned int uiDelayTime){
+	T0MR0 = uiDelayTime*15;
+	T0MCR = (Reset_MR0 | Interrupt_MR0);	
+	T0TCR = T0TCR | Counter_Reset;
+	T0TCR = T0TCR & (~Counter_Reset);
+	T0TCR = T0TCR | Counter_Enable;
+}
 	
+
+void WaitOnTimer0Match0(void){
+	while((T0IR & MR0_Interrupt_Flag) == 0){}
+		T0IR = MR0_Interrupt_Flag;
+}
 	
+
