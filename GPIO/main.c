fbc7adcad93f052b24255e26061c05eb29404c5d
Jakub Ficer <jakub.ficer@gmail.com>
2026-04-15 01:23:55 +0200

    przerwania 2

--- GPIO/main.c
+++ GPIO/main.c
@@ -1,95 +1,42 @@
-#define NULL '\0'
-#define NIBBLE_MASK 0x000F
-#define NIBBLES_IN_UI 4
+#include <LPC21xx.H>
+#include "led.h"
 
-void UIntToHexStr(unsigned int uiValue, char pcStr[]){
-	
-	unsigned char ucNibbleCounter;
-	unsigned char ucCurrentNibble;
-	
-	pcStr[0] = '0';
-	pcStr[1] = 'x';
-	pcStr[6] = NULL;
-	
-	for(ucNibbleCounter = 0; ucNibbleCounter < NIBBLES_IN_UI; ucNibbleCounter++){
-		
-		ucCurrentNibble = ((uiValue >> (ucNibbleCounter * NIBBLES_IN_UI) ) & NIBBLE_MASK);
-		
-		if(ucCurrentNibble < 10){
-			pcStr[5 - ucNibbleCounter] = ucCurrentNibble + '0';
-		}
-		else{
-			pcStr[5-ucNibbleCounter] = (ucCurrentNibble-10) + 'A';
-		}
-	}
+#define mCOUNTER_ENABLE 0x00000001
+#define mCOUNTER_RESET  0x00000002
+
+#define mINTERRUPT_ON_MR0 0x00000001
+#define mRESET_ON_MR0     0x00000002
+#define mMR0_INTERRUPT    0x00000001
+
+#define VIC_TIMER0_CHANNEL_NR 4 
+
+#define mIRQ_SLOT_ENABLE 0x00000020
+
+__irq void Timer0IRQHandler() {
+    T0IR = mMR0_INTERRUPT; 	
+    Led_StepRight();		
+    VICVectAddr = 0x00; 	
 }
 
-enum Result {OK, ERROR};
+void Timer0Interrupts_Init(unsigned int uiPeriod) { 
+    VICIntEnable |= (0x1 << VIC_TIMER0_CHANNEL_NR);            
+    VICVectCntl0  = mIRQ_SLOT_ENABLE | VIC_TIMER0_CHANNEL_NR;  
+    VICVectAddr0  = (unsigned long)Timer0IRQHandler; 	   
 
-enum Result eHexStringToUInt(char pcStr[], unsigned int *puiValue){
-	
-	unsigned char ucCharCounter;
-	unsigned char ucCurrentChar;
-	
-	if('0' != pcStr[0] | 'x' != pcStr[1] | NULL == pcStr[2]){
-		return ERROR;
-	}
-	
-	*puiValue = 0;
-	
-	for(ucCharCounter = 2; pcStr[ucCharCounter] != NULL; ucCharCounter++){
-		
-		ucCurrentChar = pcStr[ucCharCounter];
-		
-		if(6 == ucCharCounter){
-			return ERROR;
-		}
-		
-		*puiValue = *puiValue << 4;
-		
-		if(('0' <= ucCurrentChar) && ('9' >= ucCurrentChar)){
-			*puiValue = *puiValue | (ucCurrentChar - '0');
-		}
-		else if(('A' <= ucCurrentChar) && ('F' >= ucCurrentChar)){
-			*puiValue = *puiValue | (ucCurrentChar - 'A' + 10);
-		}
-		else{
-			return ERROR;
-		}
-	}
-	return OK;
+    T0MR0 = 15 * uiPeriod;                 	      
+    T0MCR |= (mINTERRUPT_ON_MR0 | mRESET_ON_MR0); 
+
+    T0TCR |=  mCOUNTER_ENABLE; 
 }
 
-void AppendUIntToString(unsigned int uiValue, char pcDestinationStr[]){
-	
-	unsigned char ucCharCounter;
-	
-	for(ucCharCounter = 0; pcDestinationStr[ucCharCounter] != NULL; ucCharCounter++){}
-	
-	UIntToHexStr(uiValue, &pcDestinationStr[ucCharCounter]);
+int main (void) {
+    unsigned int iMainLoopCtr = 0;
+    
+    LedInit();
 
-	}
-		
+    Timer0Interrupts_Init(250000);
 
-
-
-
-
-
-
-
-
-
-enum Result eStatus;
-char cHexString[] = "0xABC9";
-char cErrorString[] = "test";
-unsigned int uiValue;
-
-int main(void){
-	
-		eStatus = eHexStringToUInt(cHexString, &uiValue);
-	
-		eStatus = eHexStringToUInt(cErrorString, &uiValue);
-			
+    while(1) {
+        iMainLoopCtr++;
+    }
 }
-	
