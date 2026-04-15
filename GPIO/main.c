204bdd368fdd8583963829c03ebe226da4609653
Jakub Ficer <jakub.ficer@gmail.com>
2026-04-15 02:11:18 +0200

    przerwania 5

--- GPIO/main.c
+++ GPIO/main.c
@@ -2,12 +2,70 @@
 #include "timer_interrupts.h"
 #include "keyboard.h"
 
+void Automat(void){
+	
+			enum LedState{LED_MOVING_LEFT, LED_MOVING_RIGHT, LED_STOPPED};
+			enum LedState eLedState = LED_STOPPED;
+
+			LedInit();
+			KeyboardInit();
+
+			while(1){
+				
+				switch(eLedState){
+					
+					case LED_STOPPED:
+						
+						if(eKeyboardRead() == BUTTON_0){
+							eLedState = LED_MOVING_LEFT;
+						}		
+						
+						else if(eKeyboardRead() == BUTTON_2){
+							eLedState = LED_MOVING_RIGHT;
+						}			
+						
+						else{
+							eLedState = LED_STOPPED;
+						}
+						
+					break;
+						
+					case LED_MOVING_LEFT:
+						
+						if(eKeyboardRead() == BUTTON_1){
+							eLedState = LED_STOPPED;
+						}
+						
+						else{
+							LedStepLeft();
+							eLedState = LED_MOVING_LEFT;
+						}
+						
+					break;			
+
+					case LED_MOVING_RIGHT:
+						
+						if(eKeyboardRead() == BUTTON_1){
+							eLedState = LED_STOPPED;
+						}
+						
+						else{
+							LedStepRight();
+							eLedState = LED_MOVING_RIGHT;
+						}
+						
+					break;
+			}		
+		}
+}
+
 int main (void) {
     unsigned int iMainLoopCtr = 0;
     
     LedInit();
+		KeyboardInit();
 
-    Timer0InterruptsInit(250000,&LedStepLeft);
+    Timer0InterruptsInit(20000,&Automat);
 
     while(1) {
         iMainLoopCtr++;
