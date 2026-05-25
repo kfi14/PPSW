d398c5aa7e29dea01b61046a2b762624d3da6d24
Jakub Ficer <jakub.ficer@gmail.com>
2026-05-25 15:10:51 +0200

    uart 2

--- GPIO/main.c
+++ GPIO/main.c
@@ -8,10 +8,39 @@
 int main(void){
 	
 		unsigned int uiLoopCounter;
-		
+		extern char cOdebranyZnak;
     UART_InitWithInt(38400);
 	
     while(1) {
-  		uiLoopCounter++;	
+  		switch(cOdebranyZnak){
+				
+				case '0':
+					LedOn(0);
+					cOdebranyZnak = 0;
+				break;
+				
+				case '1':
+					LedOn(1);
+					cOdebranyZnak = 0;
+				break;
+				
+				case '2':
+					LedOn(2);
+					cOdebranyZnak = 0;
+				break;
+				
+				case '3':
+					LedOn(3);
+					cOdebranyZnak = 0;
+				break;
+				
+				case 'c':
+					LedOn(4);
+					cOdebranyZnak = 0;
+				break;
+					
+			}
+				
+			
     }
 }
